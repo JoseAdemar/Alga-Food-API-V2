8e06819a0126100e8ad949bd94a3127166a7e196
joseademar <joseademarti@gmail.com>
2024-01-02 16:35:10 -0300

    Aprendendo a utilizar os verbos HTTP em APIs REST

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,8 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +25,49 @@ public class CozinhaController {
     public List<Cozinha> listar(){
        return cozinhaRepository.todas();
     }
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml(){
+        return new CozinhasXmlWrapper(cozinhaRepository.todas());
+    }
 
     @GetMapping("/{id}")
-    public Cozinha buscar(@PathVariable  Long id){
-        return cozinhaRepository.porId(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable  Long id){
+        Cozinha cozinha =  cozinhaRepository.porId(id);
+
+        if(cozinha != null){
+            return ResponseEntity.ok(cozinha);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.adicionar(cozinha);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable("id") Long id, @RequestBody Cozinha cozinha){
+       Cozinha cozinhaAtual =  cozinhaRepository.porId(id);
+       if(cozinhaAtual != null) {
+           BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+           cozinhaAtual = cozinhaRepository.adicionar(cozinhaAtual);
+           return ResponseEntity.ok(cozinhaAtual);
+       }
+       return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cozinha> deletar(@PathVariable("id") Long id){
+        try {
+        Cozinha buscaCozinha = cozinhaRepository.porId(id);
+            if (buscaCozinha != null) {
+                cozinhaRepository.remover(buscaCozinha);
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.notFound().build();
+        }catch (DataIntegrityViolationException ex){
+           return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
